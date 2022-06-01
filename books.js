72a90e4805d7443c1ad33e4e3cc0b4c5e5b87962
Sergey Agafonov <seriywolf@gmail.com>
2022-06-01 17:07:19 +0700

    Add cancel button

--- books.js
+++ books.js
@@ -15,52 +15,50 @@ const table = document.querySelector("table");
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push({ title: title, author: author, pages: pages, read: read });
   let tr = document.createElement("tr");
-  for (let item in myLibrary[myLibrary.length-1]) {
-    let td = document.createElement("td");    
-    td.textContent = myLibrary[myLibrary.length-1][item];
-    tr.appendChild(td);    
-  };
+  for (let item in myLibrary[myLibrary.length - 1]) {
+    let td = document.createElement("td");
+    td.textContent = myLibrary[myLibrary.length - 1][item];
+    tr.appendChild(td);
+  }
   table.appendChild(tr);
-};
+}
 
 addBookToLibrary("Harry Potter", "J.Rowling", 350, true);
 addBookToLibrary("War & Peace", "L. Tolstoy", 511, false);
 
-/*
-function showLibrary(library) {
-  for (let book of library) {
-    let tr = document.createElement("tr");
-    for (let item in book) {
-      let td = document.createElement("td");
-      td.textContent = book[item];
-      tr.appendChild(td);
-    }
-    table.appendChild(tr);
-  }
-}
-*/
-//showLibrary(myLibrary);
+const newBookButton = (document.querySelector("#newBook").onclick =
+  function () {
+    document.getElementById("form").style.display = "flex";
+  });
 
 const titleInput = document.querySelector("form #title");
 const authorInput = document.querySelector("form #author");
 const pagesInput = document.querySelector("form #pages");
 const readInput = document.querySelector("form #read");
 
-const newBookButton = (document.querySelector("#newBook").onclick =
-  function () {
-    document.getElementById("form").style.display = "block";
-  });
-
 const addBookButton = (document.querySelector("#addBook").onclick =
   function () {
-    addBookToLibrary(
-      titleInput.value,
-      authorInput.value,
-      pagesInput.value,
-      readInput.checked
-    );
-    //console.log(myLibrary);
-    showLibrary(myLibrary);
-
-    //document.getElementById("form").submit();
+    if (
+      titleInput.value === "" ||
+      authorInput.value === "" ||
+      pagesInput.value === ""
+    ) {
+      alert("Error");
+    } else {
+      addBookToLibrary(
+        titleInput.value,
+        authorInput.value,
+        pagesInput.value,
+        readInput.checked
+      );
+      titleInput.value = "";
+      authorInput.value = "";
+      pagesInput.value = "";
+      readInput.checked = false;
+      document.getElementById("form").style.display = "none";
+    }
   });
+
+const cancelButton = (document.querySelector("#cancel").onclick = function () {
+  document.getElementById("form").style.display = "none";
+});
