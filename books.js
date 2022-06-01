a0ae2a747f6c1985b9ed8ea68921342abdcfb561
Sergey Agafonov <seriywolf@gmail.com>
2022-06-01 12:50:42 +0700

    Add showLibrary function for view content of library

--- books.js
+++ books.js
@@ -1,33 +1,38 @@
 let myLibrary = [];
 
-function Books (title, author,pages,read){
-    this.title=title
-    this.author=author
-    this.pages=pages
-    this.read=read
-    this.info=function(){
-        return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+function Books(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push({ title: title, author: author, pages: pages, read: read });
+}
+
+addBookToLibrary("Harry Potter", "J.Rowling", 350, true);
+addBookToLibrary("War & Peace", "L. Tolstoy", 511, false);
+
+const table = document.querySelector("table");
+
+function showLibrary(library) {
+  for (let book of library) {
+    let tr = document.createElement("tr");
+    for (let item in book) {      
+      let td = document.createElement("td");
+      td.textContent = book[item];
+      tr.appendChild(td);
     }
-};
+    table.appendChild(tr);
+  }
+}
+showLibrary(myLibrary);
 
-function addBookToLibrary(title, author,pages,read) {
-    myLibrary.push({title:title, author:author, pages:pages, read:read});
-  };
-
-  addBookToLibrary("Harry Potter", "J.Rowling", 350, true);
-  addBookToLibrary("War & Peace", "L. Tolstoy", 511, false);
- 
-  function showLibrary(library){
-      for (let book of library){
-          return book;
-      };
-
-  };
-
-  showLibrary(myLibrary);
-
-  const newBookButton = document.querySelector('#newBook').onclick = function(){
+const newBookButton = (document.querySelector("#newBook").onclick =
+  function () {
     document.getElementById("form").style.display = "block";
-  };
-
-  //const body = document.querySelector("body");
+  });
